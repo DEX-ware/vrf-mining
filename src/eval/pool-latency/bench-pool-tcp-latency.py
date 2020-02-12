4d7c9051cf227036b0c33c7fe771862d80fbff1d
HAOYUatHZ <haoyu@protonmail.com>
2020-02-12 21:45:11 +0800

    update bench-pool-tcp-latency.py

--- src/eval/pool-latency/bench-pool-tcp-latency.py
+++ src/eval/pool-latency/bench-pool-tcp-latency.py
@@ -5,32 +5,27 @@ from tcp_latency import measure_latency
 from statistics import mean
 
 def main():
-    coins = {}
+    row_dict_list = []
     with open('pools.csv') as f:
         reader = csv.DictReader(f, delimiter=',')
         for row in reader:
-            dest = {}
-            dest['host'] = row['host']
-            dest['port'] = row['port']
-            coin = row['coin']
-            if coin not in coins:
-                coins[coin] = [dest]
-            else:
-                coins[coin].append(dest)
+            row_dict = {}
+            row_dict['coin'] = row['coin']
+            row_dict['region'] = row['region']
+            row_dict['host'] = row['host']
+            row_dict['port'] = row['port']
+            row_dict_list.append(row_dict)
 
-    for coin, dests in coins.items():
-        # print(coin)
-        if coin.startswith("#"):
-            continue
-
-        for dest in dests:
-            # print(dest)
-            res = measure_latency(host=dest['host'], port=dest['port'], runs=10, timeout=2.5)
+    with open('latency.csv', 'w') as f:
+        f.write('coin,region,host,port\n')
+        for row_dict in row_dict_list:
+            # print(row_dict)
+            res = measure_latency(host=row_dict['host'], port=row_dict['port'], runs=10, timeout=2.5)
             res = [i for i in res if i] # remove None elements
             # print(res)
             try:
                 m = mean(res)
-                print("%s,%f" % (coin, m))
+                f.write("%s,%s,%s,%s,%f\n" % (row_dict['coin'], row_dict['region'], row_dict['host'], row_dict['port'], m))
             except:
                 pass
 
